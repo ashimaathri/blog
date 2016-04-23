7684ed00ee466cd545fc64008483b9221eba0868
ashima <ashimaathri@gmail.com>
2016-04-23 11:26:49 -0400

    Add templates and auto reloading

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [selmer "1.0.4"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler blog.handler/app}
+  :ring {:handler blog.handler/app
+         :auto-reload? true
+         :auto-refresh? true}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- src/blog/handler.clj
+++ src/blog/handler.clj
@@ -1,11 +1,16 @@
 (ns blog.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [selmer.parser :refer [render-file cache-off!]]))
+
+(cache-off!)
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (render-file "templates/index.html" {:title "Ashima's Blog"}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+  (wrap-defaults site-defaults)))
